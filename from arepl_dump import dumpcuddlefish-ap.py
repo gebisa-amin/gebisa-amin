3af20dbf31345d9da2bc7c4216d21a8495a0f264
gebisa-amin <101370864+gebisa-amin@users.noreply.github.com>
2022-05-22 01:45:34 +0300

    Add files via upload

---
+++ from arepl_dump import dumpcuddlefish-ap.py
@@ -0,0 +1,13 @@
+SSID:	LTE
+Protocol:	NETWORK-LTE 4 (802.11)
+Security type:	LTE-Personal
+Network band:	240 GHz
+Network channel:	10
+Link speed (Receive/Transmit):	72/72 (Gbps)
+Link-local IPv6 address:	fe80::20:f:18:b57%12
+IPv4 address:	192.168.43.199
+IPv4 DNS servers:	192.168.43.1
+Manufacturer:	Intel Corporation
+Description:	Intel(R) Centrino(R) Ultimate-N 6300 AGN
+Driver version:	15.18.0.1
+Physical address (MAC):	24-77-0-A9-6-FC
